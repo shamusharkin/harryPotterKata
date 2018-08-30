96082bdc12300d26be0e8ee5dc6436106267e735
Paul Kelly <pkelw@allstate.com>
2018-08-30 10:57:06 +0100

    restructured code and added correct tests

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -4,6 +4,7 @@ public class ShoppingCart {
 
     private final BigDecimal costOfBook= new BigDecimal(8);
     private int[] items = new int[5];
+    private int[] bookSets = new int[5];
     private BigDecimal[] discountMultipliers =  {
                 new BigDecimal("1"),
                 new BigDecimal("1"),
@@ -21,28 +22,54 @@ public class ShoppingCart {
     }
     public BigDecimal cost()
     {
-        int distinctBooks = getDistinctBooks();
-        return calcCost(discountMultipliers[distinctBooks]);
+
+        // create sets of books to be priced
+        int highestSet = getHighestSet();
+        while (highestSet > 0 )
+        {
+            // increment set count for set
+            bookSets[highestSet-1]++;
+
+            // mark books as processed for costing
+            markBooksAsProcessed();
+
+            highestSet = getHighestSet();
+        }
+
+        // now price he sets of books
+
+        return calcCost();
 
     }
 
-    private int getDistinctBooks() {
-
-        int distinctBooks =0 ;
+    void markBooksAsProcessed()
+    {
         for (int i = 0; i <5 ; i++) {
             if (items[i] >0 )
             {
-                distinctBooks++;
+                items[i]--;
             }
         }
-        return distinctBooks;
     }
 
-    private BigDecimal calcCost(BigDecimal multiplier)
+    private int getHighestSet() {
+
+        int highestSet =0 ;
+        for (int i = 0; i <5 ; i++) {
+            if (items[i] >0 )
+            {
+                highestSet++;
+            }
+        }
+        return highestSet;
+    }
+
+
+    private BigDecimal calcCost()
     {
         BigDecimal cost= new BigDecimal(0);
         for (int i = 0; i <5 ; i++) {
-            BigDecimal costOfBooks = costOfBook.multiply(new BigDecimal(items[i])).multiply(multiplier);
+            BigDecimal costOfBooks = costOfBook.multiply(new BigDecimal(bookSets[i])).multiply(new BigDecimal(i+1)).multiply( discountMultipliers[i+1]);
             cost = cost.add( costOfBooks );
         }
         return cost;
--- src/test/java/ShoppingCartTest.java
+++ src/test/java/ShoppingCartTest.java
@@ -15,18 +15,19 @@ public class ShoppingCartTest {
     }
     @Test
     public void testZeroItem() {
-        assertEquals(BigDecimal.ZERO, sc.cost());
+        BigDecimal zero = new BigDecimal("0.00");
+        assertEquals(zero, sc.cost());
     }
 
     @Test
     public void OneItem() {
-        BigDecimal eight = new BigDecimal("8");
+        BigDecimal eight = new BigDecimal("8.00");
         sc.addItem(1, 1);
         assertEquals(eight, sc.cost());
     }
     @Test
     public void TwoItemsSameBook() {
-        BigDecimal sixteen = new BigDecimal("16");
+        BigDecimal sixteen = new BigDecimal("16.00");
         sc.addItem(1, 1);
         sc.addItem(1, 1);
         assertEquals(sixteen, sc.cost());
@@ -68,4 +69,22 @@ public class ShoppingCartTest {
         sc.addItem(5, 1);
         assertEquals(thirty, sc.cost());
     }
+    @Test
+    public void fivePercentDiscountOn2BooksOnly() {
+        BigDecimal fixteenPoint2 = new BigDecimal("23.20");
+        sc.addItem(1, 1);
+        sc.addItem(2, 2);
+        assertEquals(fixteenPoint2, sc.cost());
+    }
+    @Test
+    public void lotsOfItems() {
+        BigDecimal fixteenPoint2 = new BigDecimal("51.60");
+        sc.addItem(1, 2);
+        sc.addItem(2, 2);
+        sc.addItem(3, 2);
+        sc.addItem(4, 1);
+        sc.addItem(5, 1);
+        assertEquals(fixteenPoint2, sc.cost());
+    }
+
 }
