acd914ef609477fd73457dfd807cdd0af448ddf3
Shamus <shim50@hotmail.com>
2018-10-14 09:24:09 +0100

    refactored to remove redunant lines

--- src/main/java/Books.java
+++ src/main/java/Books.java
@@ -3,14 +3,11 @@ import java.util.List;
 
 public class Books {
     private List<Integer> booklist;
-
     private static final List<String> HARRY_POTTER_BOOKS = new ArrayList<>();
+
     static {
-        HARRY_POTTER_BOOKS.add("Book1");
-        HARRY_POTTER_BOOKS.add("Book2");
-        HARRY_POTTER_BOOKS.add("Book3");
-        HARRY_POTTER_BOOKS.add("Book4");
-        HARRY_POTTER_BOOKS.add("Book5");
+        HARRY_POTTER_BOOKS.add("Book1");HARRY_POTTER_BOOKS.add("Book2");HARRY_POTTER_BOOKS.add("Book3");
+        HARRY_POTTER_BOOKS.add("Book4");HARRY_POTTER_BOOKS.add("Book5");
     }
 
     public Books() {
@@ -18,7 +15,6 @@ public class Books {
     }
 
     public void add(String book) throws Exception {
-
         if(HARRY_POTTER_BOOKS.contains(book)) {
             try {
                 booklist.set(HARRY_POTTER_BOOKS.indexOf(book), booklist.get(HARRY_POTTER_BOOKS.indexOf(book)) + 1);
@@ -28,23 +24,14 @@ public class Books {
         } else {
             throw new Exception(book + " is not a Harry Potter Book");
         }
-
     }
 
     public double getTotalPrice() {
         double totalPrice = 0;
-
         while(booklist.size() > 0) {
-            //remove any books with zero counts
             List<Integer> finalBooks = removeZeroCountBooks(booklist);
-
-            //get discount percent
             double discountPercent = getDiscount(finalBooks.size());
-
-            //decrement book count or remove book
             decrementOrRemoveBook(finalBooks);
-
-            //calculate total price
             totalPrice += calculateTotalPrice(finalBooks.size(), discountPercent);
         }
         return totalPrice;
@@ -66,7 +53,6 @@ public class Books {
     }
 
     private List<Integer> removeZeroCountBooks(List<Integer> books) {
-
         int size = books.size();
         while (size > 0) {
             for (int i=0; i<books.size(); i++){
@@ -81,17 +67,10 @@ public class Books {
     }
 
     private double getDiscount(int size) {
-
-        if (size == 2) {
-            return .95;
-        } else if (size == 3) {
-            return .90;
-        } else if (size == 4) {
-            return .80;
-        } else if (size >= 5) {
-            return .75;
-        } else {
-            return 1;
-        }
+        if (size == 2) return .95;
+        else if (size == 3) return .90;
+        else if (size == 4) return .80;
+        else if (size >= 5) return .75;
+        else return 1;
     }
 }
